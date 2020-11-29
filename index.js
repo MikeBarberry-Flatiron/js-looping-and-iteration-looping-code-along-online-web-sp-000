d4a854726d693a91e2504a99207939d73e24c418
Mike Barberry <mikebarberry@protonmail.com>
2020-11-29 05:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solutions in this file
 writeCards = (list, reason) => {
+  thankYous = []
   for (let i = 0; i < list.length; i++) {
     console.log(`Thank you, ${list[i]}, for the wonderful ${reason} gift!`)
   }
+  return thankYous
 }
 
 names = ["Chris", "Amanda", "Izzy"]
