d21baa1bb2752d52775269e7a102d75663a66096
Mike Barberry <mikebarberry@protonmail.com>
2020-11-29 05:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function writeCards(list, reason) {
+writeCards = (list, reason) => {
   for (let i = 0; i < list.length; i++) {
     console.log(`Thank you, ${list[i]}, for the wonderful ${reason} gift!`)
   }
